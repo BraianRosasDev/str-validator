54f5de69e6b6cda69631aae980a67af909396a52
Braian Rosas <braianrosas.dev@outlook.com>
2020-08-24 17:14:17 -0300

    Filter progress and validation function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-/* ------------ VALIDATION FUNCTIONS ----------- */
+/* ------------ VALIDATION FUNCTION ----------- */
 
-
+function validation(str, filter) {
+    if (filter.specialFilter) { /* For specific filters only */
+        if (str.match(filter.specific)) {
+            return true;
+        } else {
+            return false;
+        };
+    } else { /* For non-specific filters */
+        if (str.match(filter)) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+};
 
 /* --------------------------------------------- */
 /* ///////////////////////////////////////////// */
 /* -------------- GENERATE FILTER -------------- */
 
-function generateFilter(opts, cs) {
-    let filter = "[^";
+function generateFilter(opts, cs, adds) {
+    let filter = "";
+    let specific = undefined;
     if (opts.alphabet) { /* Alphabetic */
         if (cs === "lower") {
             filter = filter + "a-z";
@@ -30,7 +45,10 @@ function generateFilter(opts, cs) {
         };
     };
     if (opts.symbol) { /* Symbols */
-        filter = filter + "\\!#$%‰&'()*+,-./:;<=>?@[]^_`{|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾⅛⅜⅝⅞¿Ð×Þßæð÷þ€≠≤≥√Ω↑↓←→№↔▲►▼◄■□▪▫●○◊" + '"';
+        filter = filter + "\\!#$%‰&'*+,./:;<=>?@[\]^_`{|}~¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾⅛⅜⅝⅞¿Ð×Þßæð÷þ€≠≤≥√Ω↑↓←→№↔▲►▼◄■□▪▫●○◊\"";
+    };
+    if (opts.special) { /* Special characters */
+        filter = filter + "~!@#$%^&*_+=`|\\{}[\]:;'<>,.?/\"";
     };
     if (opts.space) { /* Space bar */
         filter = filter + " ";
@@ -39,38 +57,57 @@ function generateFilter(opts, cs) {
         filter = filter + ".,";
     };
     if (opts.paragraph) { /* Complex paragraphs */
-        filter = filter + ".,-:;()/ "
+        filter = filter + ".,:;/ "
     };
     if (opts.currency) { /* Currency */
         filter = filter + "£¥€¢$₩"
+    };
+    if (opts.password) { /* Password */
+        filter = filter + "a-zA-Z0-9áäàâãåéëèêíïìîóöòôõøúüùûçýÿñÁÄÀÂÃÅÉËÈÊÍÏÌÎÓÖÒÔÕØÚÜÙÛÇÝÑ~!@#$%^&*_+=`|\\{}[\]:;'<>,.?/\"";
+    };
+    if (opts.parentheses) {
+        filter = filter + "()";
+    };
+    if (opts.hyphen) {
+        filter = filter + "-";
     }
-};
-
-/* --------------------------------------------- */
-/* ///////////////////////////////////////////// */
-/* ------------------ RESULTS ------------------ */
-
-let results = {
-    ok: false,
-    messages: []
+    /* WARNING: From this point any of this properties overrides any filter */
+    if (opts.money) { // Money
+        specific = /^((£|¥|€|¢|\$|₩){1}([0-9]){1,3}(\,[0-9]{3}){1,}(\.[0-9]+){0,1})$|^((£|¥|€|¢|\$|₩){1}[0-9]+\.+[0-9]+)$|^((£|¥|€|¢|\$|₩){1}[0-9]+)$/gm;
+    };
+    if (opts.formalNumbers) { // Formal numbers
+        specific = /^(([0-9]){1,3}(\,[0-9]{3}){1,}(\.[0-9]+){0,1})$|^([0-9]+\.+[0-9]+)$|^([0-9]+)$/gm;
+    };
+    if (opts.email) {
+        specific = /^([a-zA-Z]{1,}[a-zA-Z0-9._-]{1,}@[a-zA-Z]{1,}\.[a-z]{1,}(\.[a-z]{1,}){0,1})$/gm;
+    };
+    /* Return filter */
+    if (specific) {
+        return { 
+            filter: specific, 
+            specialFilter: true 
+        };
+    } else {
+        return { 
+            filter: new RegExp("[^" + filter + adds + "]", "g"),
+            specialFilter: false
+        };
+    };
 };
 
 /* --------------------------------------------- */
 /* ///////////////////////////////////////////// */
 /* --------------- CORE FUNCTION --------------- */
 
-function strValidate(str, opts, cs, adds) {
-    // Generate filter
-    const filter = generateFilter(opts, cs);
-    // Call validators
-    if (alphabetOnly) onlyString(str, cs, adds);
+module.exports = {
+    
+    stringValidation(str, opts, cs, adds) {
+        // Generate filter
+        const filter = generateFilter(opts, cs, adds);
+        // Call validators
+        return validation(str, filter);
+    }
+
 };
 
 /* --------------------------------------------- */
-
-/* //////// TEST ////////// */
-strValidate("hola", {
-    alphabet: true,
-    numbers: true,
-    latin: true
-}, "lower");

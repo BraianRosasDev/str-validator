10165ad28cab7971832a8c6f281896d488e56703
Braian Rosas <braianrosas.dev@outlook.com>
2020-08-25 15:18:19 -0300

    added d.ts file

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "@axisdev/string-validation" {
+    export default function stringValidation(
+        str: string,
+        opts: object,
+        cs: string,
+        adds: string
+    ): boolean;
+};
